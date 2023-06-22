8561a0318db82d33d9496ba2e9d8257688d6ff39
miwu04 <mail@miwu04.icu>
2023-06-23 02:11:54 +0800

    venus: DerpFest bring up

--- derp_venus.mk
+++ derp_venus.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from venus device
 $(call inherit-product, device/xiaomi/venus/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
@@ -20,7 +20,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := venus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2011K2G
-PRODUCT_NAME := lineage_venus
+PRODUCT_NAME := derp_venus
+
+# DerpFest Stuffs
+TARGET_BOOT_ANIMATION_RES := 1440
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_SYSTEM_NAME := venus_global
--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ TARGET_HAS_UDFPS := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 # PowerShare
 PRODUCT_PACKAGES += \
